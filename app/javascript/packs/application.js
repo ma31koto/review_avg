7b738af37c3587f64d52aa9f2ce857d41c49157a
EC2 Default User <ec2-user@ip-172-31-22-150.ec2.internal>
2022-03-10 11:59:17 +0000

    3

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,16 +7,15 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import jQuery from "jquery"
 import "popper.js"
 import "bootstrap"
 
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-global.$ = jQuery;
-window.$ = jQuery;
+window.$ = window.jQuery = require('jquery');
+require('packs/raty')
